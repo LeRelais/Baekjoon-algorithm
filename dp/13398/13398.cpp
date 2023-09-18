3245a8fee56893177360f76521fa7da8b3cd696e
LeRelais <81596262+LeRelais@users.noreply.github.com>
2023-09-18 23:27:25 +0900

    Update 13398.cpp
    좀 더 효율적인 dp를 위한 dp의 2차원 배열로의 선언
    틀렸습니다

--- dp/13398/13398.cpp
+++ dp/13398/13398.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int arr[100001];
-int dp[100001];
+int dp[100001][2];
 
 
 int n, res = -987654321;
@@ -15,40 +15,14 @@ int main() {
 	for (int i = 1; i <= n; i++)
 		cin >> arr[i];
 
-	dp[1] = arr[1];
+	dp[1][0] = dp[1][1] = arr[1];
 
 	for (int i = 2; i <= n; i++) {
-		dp[i] = max(arr[i], dp[i - 1] + arr[i]);
-		res = max(res, dp[i]);
+		dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i]);    //이건 안빼고 그대로 진행했을 때의 누적합
+		dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i]); //이건 i번째 수를 뺐을 때의 누적합. 
+		res = max(dp[i][0], dp[i][1]);
 	}
 
-	for (int i = 1; i <= n; i++) {
-		int dp1[100001];
-		if (i == 1) {
-			dp1[2] = arr[2];
-			for (int j = 3; j <= n; j++) {
-				dp1[j] = max(arr[j], dp1[j - 1] + arr[j]);
-				res = max(res, dp1[j]);
-			}
-		}
-		else {
-			int tmp = arr[i];
-			arr[i] = -987654321;
-			dp1[1] = arr[1];
-			for (int j = 2; j <= n; j++) {
-				if (arr[j] == -987654321)
-					dp1[j] = dp1[j - 1];
-				else {
-					dp1[j] = max(dp1[j - 1] + arr[j], arr[j]);
-					res = max(res, dp1[j]);
-				}
-			}
-			arr[i] = tmp;
-		}
-		//for(int j = 1; j <= n; j++)
-		//     cout << dp1[j] << ' ';
-		// cout << endl;
-	}
 	cout << res;
 	return 0;
 }
