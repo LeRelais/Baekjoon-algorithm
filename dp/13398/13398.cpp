a6eefcb48f357e01584e4da691098cb37c363a81
LeRelais <81596262+LeRelais@users.noreply.github.com>
2023-09-18 23:27:44 +0900

    Update 13398.cpp
    맞았습니다

--- dp/13398/13398.cpp
+++ dp/13398/13398.cpp
@@ -7,7 +7,7 @@ int arr[100001];
 int dp[100001][2];
 
 
-int n, res = -987654321;
+int n, res;
 
 int main() {
 	cin >> n;
@@ -16,11 +16,12 @@ int main() {
 		cin >> arr[i];
 
 	dp[1][0] = dp[1][1] = arr[1];
+	res = arr[1];
 
 	for (int i = 2; i <= n; i++) {
 		dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i]);    //이건 안빼고 그대로 진행했을 때의 누적합
 		dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i]); //이건 i번째 수를 뺐을 때의 누적합. 
-		res = max(dp[i][0], dp[i][1]);
+		res = max(res, max(dp[i][0], dp[i][1]));
 	}
 
 	cout << res;
