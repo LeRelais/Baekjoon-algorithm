b5b5745668db038a7571eb4c9fad93df06c8d8b8
hyeonjun <lerelais0102@naver.com>
2024-02-23 13:30:26 +0900

    오답

---
+++ 그래프/2668.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int n;
+bool flag;
+int arr[101], used[101];
+vector<int> v;
+
+void dfs(int start, int next) {
+    if (used[next] == 1) {
+        if (start == next) {
+            v.push_back(start);
+            v.push_back(next);
+            flag = true;
+        }
+        return;
+    }
+
+    used[arr[next]] = 1;
+    dfs(start, arr[next]);
+
+    if (flag) {
+        v.push_back(start);
+        v.push_back(next);
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        used[i] = 1;
+        dfs(i, arr[i]);
+        memset(used, false, sizeof(used));
+        flag = false;
+    }
+
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+
+
+    cout << v.size() << '\n';
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << '\n';
+    }
+    return 0;
+}
