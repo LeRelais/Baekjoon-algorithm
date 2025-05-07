c6daeaed199358e87ce979076a3460a679452367
LeRelais <lerelais0102@naver.com>
2025-05-07 09:40:08 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c, d, e, f;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	int y = (c * d - a * f) / (b * d - a * e);
+	int x = (c * e - b * f) / (a * e - b * d);
+	
+	printf("%d %d\n", x, y);
+	return 0;
+}
