1a094b1f9da0494a9906e279ecff7f0d7b1cae57
LeRelais <lerelais0102@naver.com>
2025-09-06 19:27:36 +0900

    [Silver I] Title: 1로 만들기 2, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.cc
@@ -1,62 +1,58 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <deque>
-
 using namespace std;
 
-const int INF = 987654321; // 무한대를 나타내는 큰 값
+int dp[1000001], trace[1000001], n;
+deque<int> dq;
+
+void init(){
+    cin >> n;
+    
+    for(int i = 1; i <= n; i++){
+        dp[i] = 987654321;
+    }
+}
+
+void solve(){
+    dp[1] = 0;
+    
+    for(int i = 1; i < n; i++){
+        if(dp[i] + 1 < dp[i+1]){
+            dp[i+1] = dp[i] + 1;
+            trace[i+1] = i;
+        }
+        
+        if(dp[i] + 1 < dp[i*2] && i * 2 <= n){
+            dp[i*2] = dp[i] + 1;
+            trace[i*2] = i; 
+        }
+        
+        if(dp[i] + 1 < dp[i*3] && i * 3 <= n){
+            dp[i*3] = dp[i] + 1;
+            trace[i*3] = i; 
+        }
+    }
+    
+    int cur = n;
+    
+    while(cur != 0){
+        dq.push_back(cur);
+        cur = trace[cur];
+    }
+    
+    cout << dp[n] << '\n';
+    
+    while(!dq.empty()){
+        cout << dq.front() << ' ';
+        dq.pop_front();
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int n;
-    cin >> n;
-
-    // dp[i] : i를 1로 만드는 최소 연산 횟수
-    // trace[i] : i로 가기 직전의 수
-    vector<int> dp(n + 1, INF);
-    vector<int> trace(n + 1, 0);
-
-    dp[1] = 0;
-
-    for (int i = 1; i < n; ++i) {
-        // 현재 수 i에서 1을 더하는 연산
-        if (dp[i] + 1 < dp[i + 1]) {
-            dp[i + 1] = dp[i] + 1;
-            trace[i + 1] = i;
-        }
-
-        // 현재 수 i에서 2를 곱하는 연산
-        if (i * 2 <= n && dp[i] + 1 < dp[i * 2]) {
-            dp[i * 2] = dp[i] + 1;
-            trace[i * 2] = i;
-        }
-        
-        // 현재 수 i에서 3을 곱하는 연산
-        if (i * 3 <= n && dp[i] + 1 < dp[i * 3]) {
-            dp[i * 3] = dp[i] + 1;
-            trace[i * 3] = i;
-        }
-    }
-
-    // 결과 출력
-    cout << dp[n] << '\n';
-
-    // 경로 역추적 및 출력
-    deque<int> path;
-    int current = n;
-    while (current != 0) {
-        path.push_back(current);
-        current = trace[current];
-    }
-    
-    while (!path.empty()) {
-        cout << path.front() << " ";
-        path.pop_front();
-    }
-    cout << '\n';
-
-    return 0;
+    init();    
+    solve();
 }
