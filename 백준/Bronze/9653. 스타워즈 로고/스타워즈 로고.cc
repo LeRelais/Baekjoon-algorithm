a82bb35a86308e70a786f924514ec6e8739db776
LeRelais <lerelais0102@naver.com>
2025-09-28 17:55:13 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 입출력 속도 향상 (선택 사항이지만 좋은 습관입니다)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // 첫 번째 블록 출력
+    cout << "    8888888888  888    88888" << endl;
+    cout << "   88     88   88 88   88  88" << endl;
+    cout << "    8888  88  88   88  88888" << endl;
+    cout << "       88 88 888888888 88   88" << endl;
+    cout << "88888888  88 88     88 88    888888" << endl;
+    
+    cout << "" << endl; // 빈 줄 출력
+
+    // 두 번째 블록 출력
+    cout << "88  88  88   888    88888    888888" << endl;
+    cout << "88  88  88  88 88   88  88  88" << endl;
+    cout << "88 8888 88 88   88  88888    8888" << endl;
+    cout << " 888  888 888888888 88  88      88" << endl;
+    cout << "  88  88  88     88 88   88888888" << endl;
+
+    return 0;
+}
