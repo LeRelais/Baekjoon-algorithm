4a193a5c5d412f3b19aae9e00c579ba85ccc6250
LeRelais <lerelais0102@naver.com>
2025-09-30 23:07:17 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+int main() {
+    char str[21];
+    scanf("%s", str);
+    printf(":fan::fan::fan:\n:fan::%s::fan:\n:fan::fan::fan:", str);
+}
