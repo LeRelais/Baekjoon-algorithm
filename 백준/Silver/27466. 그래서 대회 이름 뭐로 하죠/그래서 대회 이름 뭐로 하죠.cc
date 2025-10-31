4197b17f50130ba319687ec48ebd0aff4ae8352f
LeRelais <lerelais0102@naver.com>
2025-10-31 13:44:42 +0900

    [Silver IV] Title: 그래서 대회 이름 뭐로 하죠, Time: 0 ms, Memory: 2912 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Silver/27466. 그래서 대회 이름 뭐로 하죠/그래서 대회 이름 뭐로 하죠.cc
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    string str;
+    cin >> str;
+    int k;
+    string tmp = "";
+    for(int i = str.length()-1; i >= 0; i--){
+        if(str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U'){
+            k = i;
+            tmp += str[i];
+            break;
+        }
+    }
+    
+    int flag = 0;
+    for(int i = k-1; i >= 0; i--){
+        if(str[i] == 'A')
+        {
+            k = i;
+            tmp += str[i];
+            flag = 1;
+            break;
+        }
+    }
+    if(!flag)
+    {
+        cout << "NO";
+        return 0;
+    }
+    
+    flag = 0;
+    for(int i = k-1; i >= 0; i--){
+        if(str[i] == 'A')
+        {
+            k = i;
+            tmp += str[i];
+            flag = 1;
+            break;
+        }
+    }
+    if(!flag)
+    {
+        cout << "NO";
+        return 0;
+    }
+    
+    for(int i = k-1; i >= 0; i--){
+        if(str[i] != 'A' && str[i] != 'E' && str[i] != 'I' && str[i] != 'O' && str[i] != 'U'){
+            tmp += str[i];
+            if(tmp.length() == m)
+                break;
+        }
+    }
+    reverse(tmp.begin(), tmp.end());
+    if(tmp.length() < m){
+        cout << "NO";
+        return 0;
+    }
+    cout << "YES\n" << tmp;
+    return 0;
+}
