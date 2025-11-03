b8b04d17131fd321b937ac3fd5cbd8cad37d33d9
LeRelais <lerelais0102@naver.com>
2025-11-03 11:11:47 +0900

    [Gold V] Title: 골목 대장 호석 - 기능성, Time: 0 ms, Memory: 2168 KB, Score: 8 point -BaekjoonHub

--- 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.cc
+++ 백준/Gold/20168. 골목 대장 호석 － 기능성/골목 대장 호석 － 기능성.cc
@@ -7,7 +7,7 @@ using namespace std;
 
 int n, m, a, b, c;
 vector<vector<pair<int, int>>> graph;
-vector<int> dist;
+vector<int> sanity, dist;
 
 typedef struct _data{
     int san;
@@ -21,14 +21,12 @@ struct cmp{
   }  
 };
 
-//문제 input 입력받기, 그 외 사용할 vector 및 변수 초기화 작업 진행
 void init(){
     cin >> n >> m >> a >> b >> c;
     int from, to, value;
     graph.resize(n+1);
     dist.resize(n+1, 987654321);
-    
-    // 문제에선 양방향이라고 언급했지만 이미 지나간 길을 다시 지나갈 일이 없기 때문에 단방향 그래프 구성
+    sanity.resize(c+1, 987654321);
     for(int i = 0; i < n; i++){
         cin >> from >> to >> value;
         graph[from].push_back({to, value});
@@ -37,7 +35,7 @@ void init(){
 
 void solve(){
     priority_queue<DATA, vector<DATA>, cmp> pq;
-    pq.push({0,0,1});
+    pq.push({0,0,a});
     int res = 987654321;
     while(!pq.empty()){
         int san = pq.top().san;
@@ -54,7 +52,7 @@ void solve(){
             int next = graph[x][i].first;
             int n_cost = graph[x][i].second;
             
-            if(dist[next] > cost + n_cost && cost + n_cost <= c){
+            if(cost + n_cost <= c){
                 dist[next] = cost + n_cost;
                 int n_san = max(san, n_cost);
                 pq.push({n_san, dist[next], next});
