afdaa2e97b5e5b249ed5ea72f0cab482f6c48648
LeRelais <lerelais0102@naver.com>
2025-11-24 18:53:58 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	string str;
+
+	while (getline(cin, str)) {
+		cout << str << '\n';
+	}
+}
