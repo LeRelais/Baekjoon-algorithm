e9fb65405683885b5cbf423cfa926ae6de6b81fa
LeRelais <lerelais0102@naver.com>
2026-01-29 20:09:12 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/README.md
+++ 프로그래머스/2/148653. 마법의 엘리베이터/README.md
@@ -1,10 +1,10 @@
 # [level 2] 마법의 엘리베이터 - 148653 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/148653#) 
 
 ### 성능 요약
 
-메모리: 3.7 MB, 시간: 0.02 ms
+메모리: 4.03 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 28일 17:01:59
+2026년 01월 29일 20:09:15
 
 ### 문제 설명
 
--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -3,52 +3,95 @@
 
 using namespace std;
 
-struct Node {
+typedef struct Node {
     int cur;
     int cost;
-};
+}NODE;
 
 int solution(int storey) {
-    queue<Node> q;
+    int answer = 1e9;
+    queue<NODE> q;
     q.push({storey, 0});
+    unordered_map<int, int> m;
+    m[storey] = 0;
     
-    // dist[층수] = 최소 돌 개수
-    unordered_map<int, int> dist;
-    dist[storey] = 0;
-    
-    int min_stone = 1e9;
-    
-    while(!q.empty()) {
-        Node node = q.front();
+    while(!q.empty()){
+        int cur = q.front().cur;
+        int cost = q.front().cost;
         q.pop();
         
-        // 이미 더 적은 비용으로 도달한 적이 있다면 무시
-        if (dist.count(node.cur) && dist[node.cur] < node.cost) continue;
-        if (node.cost >= min_stone) continue;
-
-        if (node.cur == 0) {
-            min_stone = min(min_stone, node.cost);
+        if(m.count(cur) && m[cur] < cost)
+            continue;
+        
+        if(cur == 0){
+            answer = min(answer, cost);
             continue;
         }
         
-        int last = node.cur % 10;
+        int digit = cur % 10;
+        int next = cur / 10;
         
-        // 1. 내림
-        int next1 = node.cur / 10;
-        int cost1 = node.cost + last;
-        if (!dist.count(next1) || dist[next1] > cost1) {
-            dist[next1] = cost1;
-            q.push({next1, cost1});
+        if(!m.count(next) || m[next] > cost + digit){
+            q.push({next, cost + digit});
+            m[next] = cost + digit;
         }
         
-        // 2. 올림
-        int next2 = node.cur / 10 + 1;
-        int cost2 = node.cost + (10 - last);
-        if (!dist.count(next2) || dist[next2] > cost2) {
-            dist[next2] = cost2;
-            q.push({next2, cost2});
+        if(!m.count(next+1) || m[next+1] > cost + (10 - digit)){
+            q.push({next + 1, cost + (10 - digit)});
+            m[next+1] = cost + (10-digit);
         }
     }
     
-    return min_stone;
+    
+    
+    return answer;
+    
+    
+    
+    
+    
+    
+    
+//     queue<Node> q;
+//     q.push({storey, 0});
+    
+//     // dist[층수] = 최소 돌 개수
+//     unordered_map<int, int> dist;
+//     dist[storey] = 0;
+    
+//     int min_stone = 1e9;
+    
+//     while(!q.empty()) {
+//         Node node = q.front();
+//         q.pop();
+        
+//         // 이미 더 적은 비용으로 도달한 적이 있다면 무시
+//         if (dist.count(node.cur) && dist[node.cur] < node.cost) continue;
+//         if (node.cost >= min_stone) continue;
+
+//         if (node.cur == 0) {
+//             min_stone = min(min_stone, node.cost);
+//             continue;
+//         }
+        
+//         int last = node.cur % 10;
+        
+//         // 1. 내림
+//         int next1 = node.cur / 10;
+//         int cost1 = node.cost + last;
+//         if (!dist.count(next1) || dist[next1] > cost1) {
+//             dist[next1] = cost1;
+//             q.push({next1, cost1});
+//         }
+        
+//         // 2. 올림
+//         int next2 = node.cur / 10 + 1;
+//         int cost2 = node.cost + (10 - last);
+//         if (!dist.count(next2) || dist[next2] > cost2) {
+//             dist[next2] = cost2;
+//             q.push({next2, cost2});
+//         }
+//     }
+    
+//     return min_stone;
 }
