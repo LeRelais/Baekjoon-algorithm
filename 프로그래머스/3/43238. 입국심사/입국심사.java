18fad0ba576103121c993e843454869e4b5df013
LeRelais <lerelais0102@naver.com>
2026-02-19 16:23:32 +0900

    [level 3] Title: 입국심사, Time: 64.24 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,13 +1,11 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        long low = 1;
-        long high = (long)1e9 * (long)1e9;
-        
+        long low = 1, high = (long)(1e9 * 1e5);
+        long answer = 1;
         while(low <= high){
             long mid = (low + high) / 2;
-            long cnt = 0;
             
+            long cnt = 0;
             for(int i = 0; i < times.length; i++){
                 cnt += mid / times[i];
                 if(cnt >= n)
@@ -18,10 +16,10 @@ class Solution {
                 answer = mid;
                 high = mid - 1;
             }
-            else{
+            else
                 low = mid + 1;
-            }
         }
+        
         return answer;
     }
 }
